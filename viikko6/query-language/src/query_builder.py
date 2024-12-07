1535cab8f372d3db04a600bbc43a9f2d71caab1e
Merentie <mikael.merentie@gmail.com>
2024-12-08 00:16:37 +0200

    Add one of operator to QueryBuilder

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -13,5 +13,8 @@ class QueryBuilder:
     def has_fewer_than(self, value, attribute):
         return QueryBuilder(And(self.query, HasFewerThan(value, attribute)))
 
+    def one_of(self, q1, q2):
+        return QueryBuilder(Or(q1,q2))
+
     def build(self):
         return self.query
